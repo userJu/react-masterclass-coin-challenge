ef51f8f234221f6086fc6631c41129deddf9cc79
JuhyunLim <87933367+userJu@users.noreply.github.com>
2022-01-27 23:09:41 +0900

    make chart and price basic

---
+++ src/routes/Coins.tsx
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Container = styled.div`
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${(props) => props.theme.accentColor};
+`;
+
+const Header = styled.header`
+  height: 15vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CoinsList = styled.ul``;
+
+const Coin = styled.li`
+  background-color: white;
+  color: ${(props) => props.theme.bgColor};
+  border-radius: 15px;
+  margin-bottom: 10px;
+  a {
+    display: flex;
+    align-items: center;
+    padding: 20px;
+    transition: color 0.2s ease-in;
+    display: block;
+  }
+  &:hover {
+    a {
+      color: ${(props) => props.theme.accentColor};
+    }
+  }
+`;
+const Img = styled.img`
+  width: 35px;
+  height: 35px;
+  margin-right: 10px;
+`;
+
+interface CoinInterface {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+}
+
+function Coins() {
+  const [coins, setCoins] = useState<CoinInterface[]>([]);
+  return (
+    <Container>
+      <Header>
+        <Title>코인</Title>
+      </Header>
+      <CoinsList>
+        {coins.map((coin) => (
+          <Coin key={coin.id}>
+            <Link
+              to={`/${coin.id}`}
+              state={{ name: coin.name, rank: coin.rank }}
+            >
+              <img
+                src={`https://cryptoicon-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`}
+                alt=""
+              />
+              {coin.name}
+            </Link>
+          </Coin>
+        ))}
+      </CoinsList>
+    </Container>
+  );
+}
+export default Coins;
